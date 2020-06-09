1d2ff7b2b665148fc8304a3e5e1f65be4d3319d1
Dudi <dudiko2@gmail.com>
2020-06-09 13:41:46 +0300

    Article component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-	return (
-		<div className="App">
-			<h1>News-App</h1>
-		</div>
-	);
+	return <div className="App">News App</div>;
 }
 
 export default App;
---
+++ src/components/Article.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Article = ({
+	title = "title",
+	author = "author",
+	date = "date",
+	desc = "lorem",
+	source = "src",
+}) => {
+	const bgStyles = {
+		padding: "2em",
+		backgroundColor: "#fff",
+		margin: "1em",
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "flex-start",
+		color: "#707070",
+		maxWidth: "700px",
+	};
+
+	const titleStyles = {
+		margin: "0 0 .3em 0",
+		fontSize: "1.65em",
+	};
+
+	const authorDateStyles = {
+		fontSize: "0.65em",
+		margin: "0 0 .6em 0",
+	};
+
+	return (
+		<div style={bgStyles}>
+			<h1 style={titleStyles}>{title}</h1>
+			<div style={authorDateStyles}>
+				<p>{author}</p>
+				<p>{date}</p>
+			</div>
+			<p style={{ margin: "0 0 .6em 0" }}>{desc}</p>
+			<a href="/">{source}</a>
+		</div>
+	);
+};
+
+export default Article;
