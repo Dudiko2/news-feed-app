2b7c5c00e5198946df77e8eeaf988ac8941a5da6
Dudi <dudiko2@gmail.com>
2020-06-22 15:29:13 +0300

    signup styles; should make a unified component for the forms

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,29 +1,11 @@
 import React, { useState } from "react";
+import Styles from "./SignUpForm.module.css";
 import api from "../../axios";
 
 const Form = ({ setAuth }) => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [confirmPassword, setConfirmPassword] = useState("");
-
-	const bgStyle = {
-		display: "flex",
-		flexDirection: "column",
-		justifyContent: "center",
-		padding: "2em",
-	};
-
-	const formStyle = {
-		display: "flex",
-		flexDirection: "column",
-	};
-
-	const inputStyle = {
-		marginTop: "1em",
-		height: "4em",
-		width: "20em",
-		fontSize: "1em",
-	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -42,31 +24,28 @@ const Form = ({ setAuth }) => {
 	};
 
 	return (
-		<div style={bgStyle}>
-			<h2 style={{ marginBottom: "1em" }}>Sign Up</h2>
-			<form style={formStyle} onSubmit={handleSubmit}>
+		<div className={Styles.bgStyle}>
+			<h2>Sign Up</h2>
+			<form className={Styles.formStyle} onSubmit={handleSubmit}>
 				<input
-					style={inputStyle}
 					type="text"
 					placeholder="email"
 					onChange={(e) => setEmail(e.target.value)}
 					value={email}
 				></input>
 				<input
-					style={inputStyle}
 					type="password"
 					placeholder="password"
 					onChange={(e) => setPassword(e.target.value)}
 					value={password}
 				></input>
 				<input
-					style={inputStyle}
 					type="password"
 					placeholder="confirm password"
 					onChange={(e) => setConfirmPassword(e.target.value)}
 					value={confirmPassword}
 				></input>
-				<input style={inputStyle} type="submit" value="Send"></input>
+				<input type="submit" value="Send"></input>
 			</form>
 		</div>
 	);
