74f58c6480acf19e4eba0b1261d00c297d16e7ca
Dudi <dudiko2@gmail.com>
2020-07-01 16:38:00 +0300

    news api refactor

--- server/api/news.js
+++ server/api/news.js
@@ -1,16 +1,21 @@
 const axios = require("axios");
-const { makeQString, isEmptyObj } = require("../../lib/lib");
+const { makeQString } = require("../../lib/lib");
 
 const newsAxios = axios.create({
 	baseURL: "https://newsapi.org/v2",
 	headers: { Authorization: process.env.NEWS_API_KEY },
 });
 
+const hasReqKey = (query) => {
+	const reqKeys = ["sources", "q", "language", "country", "category"];
+	return reqKeys.findIndex((k) => query[k]) !== -1;
+};
+
 const news = {
 	getHeadlines(query) {
 		let qString;
-		if (!query || isEmptyObj(query)) {
-			qString = makeQString({ country: "us" });
+		if (!query || !hasReqKey(query)) {
+			qString = makeQString({ country: "us", ...query });
 		} else {
 			qString = makeQString(query);
 		}
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import api from "../../axios";
 import PulseLoader from "react-spinners/PulseLoader";
 import Article from "../../components/Article/Article";
@@ -7,24 +7,33 @@ const Main = () => {
 	const [articles, setArticles] = useState([]);
 	const [isLoading, setLoading] = useState(true);
 
-	useEffect(() => {
-		api.get("/news/top").then((r) => {
-			setArticles(r.data.articles);
+	const getTopNews = useCallback((page) => {
+		api.get(`/news/top?page=${page}`).then((r) => {
+			setArticles((articles) => articles.concat(r.data.articles));
 			setLoading(false);
 		});
 	}, []);
 
+	useEffect(() => {
+		getTopNews(1);
+	}, [getTopNews]);
+
 	return (
 		<>
 			<main style={{ gridColumn: "2 / 3" }}>
-				{isLoading ? (
-					<PulseLoader
-						css="display: flex; justify-content: center; align-items: center; height: 100%; margin-top: 1em;"
-						color="var(--dom)"
-					/>
-				) : (
-					articles.map((a) => <Article article={a} key={a.url} />)
-				)}
+				{articles.map((a, index) => (
+					<Article article={a} key={a.url + index} />
+				))}
+				<PulseLoader
+					css={`
+						display: ${isLoading ? "flex" : "none"};
+						justify-content: center;
+						align-items: center;
+						height: 100%;
+						margin-top: 1em;
+					`}
+					color="var(--dom)"
+				/>
 			</main>
 			<div>
 				<div
