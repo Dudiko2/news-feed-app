ed0080cc774b46956aa1231d5f7fcc69ebb29b4f
Dudi <dudiko2@gmail.com>
2020-07-08 11:34:24 +0300

    account styling

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -1,15 +1,34 @@
 import React, { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
+import Styles from "./Account.module.css";
+
+const dict = {
+	langs: {
+		he: "Hebrew",
+		en: "English",
+	},
+};
 
 const Account = () => {
 	const { userData } = useContext(AuthContext);
+
+	const languages = userData.langs
+		.filter((l) => dict.langs[l])
+		.map((l) => <li>{dict.langs[l]}</li>);
+
+	const sources = userData.sources.map((s) => <li>{s}</li>);
+
 	return (
-		<div style={{ gridColumn: "2/3" }}>
-			<div style={{ padding: "2em", backgroundColor: "var(--antidom)" }}>
-				<h2 style={{ fontWeight: "normal" }}>{userData.email}</h2>
+		<div className={Styles.Account}>
+			<div className={Styles.Cont}>
+				<h2>{userData.email}</h2>
 				<div>
 					<h3>Preferred languages</h3>
-					<p>{userData.langs}</p>
+					<ul>{languages}</ul>
+				</div>
+				<div>
+					<h3>Following</h3>
+					<ul>{sources}</ul>
 				</div>
 			</div>
 		</div>
